e3ec99518a8c62bee53e448920b3d53224a6081b
Anastasia_Kiryanova <a.kiryanova99@yandex.ru>
2021-12-20 23:32:39 +0500

    first commit

---
+++ js/slider.js
@@ -0,0 +1,13 @@
+const slider = function () {
+  const swiper = new Swiper(".swiper-container", {
+			loop: true,
+			navigation: {
+				nextEl: ".slider-button-next",
+				prevEl: ".slider-button-prev",
+			},
+		});
+};
+
+slider();
+ 
+ 
